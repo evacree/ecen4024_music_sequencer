20ec3ff438ad245ce08a611be378e821741ef85e
landerich <ricland@okstate.edu>
2026-01-29 22:20:51 -0600

    Update hand_landmarker2.py

--- hand_landmarker2.py
+++ hand_landmarker2.py
@@ -16,7 +16,7 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 500)
 
-mp_drawing = mp.solutions.drawing_utils
+mp_drawing = mp.solutions.drawing_utils # Imports mediapipe solutions and vision
 mp_hands = mp.solutions.hands
 hand = mp_hands.Hands(static_image_mode=False,
     max_num_hands=2,
@@ -28,20 +28,20 @@ while True:
     success, frame = cap.read()
     frame = cv2.flip(frame, 1)
     if success:
-        RGB_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        RGB_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # RGB conversion
         result = hand.process(RGB_frame)
         if result.multi_hand_landmarks :
-            for hand_landmarks in result.multi_hand_landmarks:
+            for hand_landmarks in result.multi_hand_landmarks: # Two hands tracking
 
                 coords = []
                 for lm in hand_landmarks.landmark:
                     coords.extend([lm.x, lm.y, lm.z])
-                gesture = data.predict([coords])[0]
+                gesture = data.predict([coords])[0] # Taking coordinates and taking info
                 cv2.putText(frame, gesture, (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS) # Draws joint lines
         cv2.imshow("capture image", frame)
-        if cv2.waitKey(1) == ord('q'):
+        if cv2.waitKey(1) == ord('q'): # If there is a key in this case q quits the program
             break
 cap.release()
 cv2.destroyAllWindows()    
