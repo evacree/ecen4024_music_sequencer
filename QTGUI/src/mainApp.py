8def222ce4152dbab119e7350f882fd2e12db67b
Abbie Schlatter <abbie.schlatter@okstate.edu>
2026-04-22 19:01:27 -0500

    idk bro

--- QTGUI/src/mainApp.py
+++ QTGUI/src/mainApp.py
@@ -2,7 +2,7 @@
 # 1080p-friendly window size (fits your screen perfectly)
 
 from PyQt6 import QtWidgets, QtCore, QtGui
-from finalgui import Ui_MainWindow
+from finalguiRescalable import Ui_MainWindow
 from sequencer import GestureSequencer
 import cv2
 import os
